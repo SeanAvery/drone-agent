7c7f8c02f0e79ddef129b89ed52e02b5880f6300
Sean Pollock <seanpollock3344@gmail.com>
2018-03-30 10:20:28 -0400

    fixing tilting. Adding rotation movements

--- DroneMovements.cs
+++ DroneMovements.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,8 +12,11 @@ public class DroneMovement : MonoBehaviour {
 	void FixedUpdate(){
 		MovementUpDown ();
 		MovementForward ();
+		Rotation ();
 
 		drone.AddRelativeForce (Vector3.up * upForce);
+		drone.rotation = Quaternion.Euler(
+			new Vector3(tiltAmount, currentYRotation, drone.rotation.z));
 	}
 
 	public float upForce;
@@ -27,12 +31,30 @@ public class DroneMovement : MonoBehaviour {
 	}
 
 	public float movementForwardForce = 500;
-	public float tiltAmount;
+	public float tiltAmount = 0;
 	public float tiltVelocity;
 
 	void MovementForward() {
 		if (Input.GetAxis ("Vertical") != 0) {
-			drone.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * movementForwardForce) ;
+			drone.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * movementForwardForce);
+			tiltAmount = Mathf.SmoothDamp (tiltAmount, 20 * Input.GetAxis("Vertical"), ref tiltVelocity, 0.1f);
 		}
+	}
+
+	public float wantedYRotation;
+	public float currentYRotation;
+	public float rotateAmountByKeys = 2.5f;
+	public float rotationYVelocity;
+
+	void Rotation() {
+		if (Input.GetKey (KeyCode.J)) {
+			wantedYRotation -= rotateAmountByKeys;
+		}
+		if (Input.GetKey (KeyCode.K)) {
+			wantedYRotation += rotateAmountByKeys;
+		}
+
+		currentYRotation = Mathf.SmoothDamp (currentYRotation, wantedYRotation, ref rotationYVelocity, 0.25f);
+
 	}
 }
