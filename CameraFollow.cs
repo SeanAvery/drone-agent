39d6460b01e4fbe6ee8f9de6e5863de956f6735a
Sean Pollock <seanpollock3344@gmail.com>
2018-04-03 13:03:31 -0400

    camera rotation

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -10,8 +10,11 @@ public class CameraFollow : MonoBehaviour {
 	}
 
 	public Vector3 cameraVelocity;
-	public Vector3 behindPosition = new Vector3(0,2,-4)
+	public Vector3 behindPosition = new Vector3(0,2,-4);
+	public float cameraAngle = -10;
+
 	void FixedUpdate() {
 		transform.position = Vector3.SmoothDamp(transform.position, drone.transform.TransformPoint(behindPosition) + Vector3.up * Input.GetAxis("Vertical"), ref cameraVelocity, 0.1f);
+		transform.rotation = Quaternion.Euler (new Vector3 (cameraAngle, drone.GetComponent<DroneMovement> ().currentYRotation, 0));
 	}
 }
