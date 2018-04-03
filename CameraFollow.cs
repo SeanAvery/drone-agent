012b55d2f1842d9183853fcb56c09c68afe006fb
Sean Pollock <seanpollock3344@gmail.com>
2018-04-03 12:50:46 -0400

    adding camera follow veolocity

---
+++ CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform drone;
+
+	void Awake() {
+		drone = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	public Vector3 cameraVelocity;
+	public Vector3 behindPosition = new Vector3(0,2,-4)
+	void FixedUpdate() {
+		transform.position = Vector3.SmoothDamp(transform.position, drone.transform.TransformPoint(behindPosition) + Vector3.up * Input.GetAxis("Vertical"), ref cameraVelocity, 0.1f);
+	}
+}
