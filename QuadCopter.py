605da598a21b5cfe126b0e37093f52f87bc59c31
Sean Pollock <seanpollock3344@gmail.com>
2018-04-11 11:52:10 -0400

    initializing position and velocity

---
+++ QuadCopter.py
@@ -0,0 +1,10 @@
+import math
+import numpy as np
+
+class QuadCopter():
+    def __init__(self):
+        self.position = (0, 0, 0)
+        self.velocity = (0, 0, 0)
+
+if __name__ == '__main__':
+    copter = QuadCopter()
