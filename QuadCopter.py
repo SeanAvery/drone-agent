96bc48cc3e54e7af19bc5c4a28f3d4964b30fd5b
Sean Pollock <seanpollock3344@gmail.com>
2018-04-12 12:09:25 -0400

    calc torque with given motor torque constant

--- QuadCopter.py
+++ QuadCopter.py
@@ -6,7 +6,18 @@ class QuadCopter():
         self.position = (0, 0, 0)
         self.velocity = (0, 0, 0)
         self.angle = (0, 0, ) # pitch, yaw, roll
-        self.angle_velocity = (0, 0, )
+        self.angle_velocity = (0, 0, 0)
 
+class Rotor():
+    def __init__(self, motor_torque_constant):
+        self.motor_torque_constant = motor_torque_constant
+        self.initial_current = 0
+        self.current = 0
+        Aself.torque = self.calc_torque()
+
+    def calc_torque(self):
+        return self.motor_torque_constant * (self.current - self.initial_current)
+        
 if __name__ == '__main__':
+    motor_torque_constant = 1000
     copter = QuadCopter()
